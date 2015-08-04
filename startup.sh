e7e079519ef8eed07bc4c7096996c7d4639ba828
Martin Verspai <martin.verspai@iteratec.de>
2015-08-04 14:13:13 +0200

    Added Java opts for automatic garbage collection;

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export JAVA_OPTS="-Xms256m -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djava.net.preferIPv4Stack=true"
+
 if [ -n "$PASS" ]
 then
         echo publisher:$PASS | chpasswd
