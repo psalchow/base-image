8e559903597ee6599aac437b79849906a85553a4
fbe <fbe@LT526.iteratec.de>
2015-10-09 13:31:41 +0200

    switch for development environment removed

--- startup.sh
+++ startup.sh
@@ -2,10 +2,4 @@
 
 export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djava.net.preferIPv4Stack=true"
 
-if [ -n "$ENV" ]
-then
-        cp /opt/jboss/configuration/standalone_$ENV.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
-        echo "Copied standalone_$ENV.xml to configuration folder"
-fi
-
 /opt/jboss/wildfly/bin/standalone.sh -P=/opt/jboss/additionalSystemProperties.properties
